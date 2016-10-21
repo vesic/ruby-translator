6b23090e3777f6e787aa3c215b15b70c2dbdc279
dusan.vesic <dusan.vesic@gmail.com>
2016-10-21 16:23:26 +0200

    Add menu options

--- merge-files.rb
+++ merge-files.rb
@@ -1,23 +1,45 @@
+require 'highline'
 require 'colorize'
 
-file1 = ARGV[0]
-file2 = ARGV[1]
-output_file = ARGV[2]
+cli = HighLine.new
+files = []
+want_more = true
+output_file = nil
 
-files = [file1, file2]
+while want_more
+  cli.choose do |menu|
+    menu.prompt = "What's next?"
+    menu.choice(:input) do 
+      file = cli.ask('Input file name:')
+      files << file
+      puts
+    end
+    menu.choice(:output) do
+      output_file = cli.ask('File to write combined input:')
+      puts
+    end
+    menu.choice('current status') do
+      puts "Input files: #{files.join(', ').colorize(:blue)}"
+      puts "Output file: #{output_file.nil? ? 'none' : output_file.colorize(:blue)}"
+      puts
+    end
+    menu.choice('Quit and save') do
+      want_more = false
+      combined = []
 
-combined = []
+      files.each do |file|
+        File.open(file).each do |row|
+          combined << row.strip
+        end
+      end
 
-files.each do |file|
-  File.open(file).each do |row|
-    combined << row.strip
-  end
-end
-
-File.open(output_file, 'w') do |file|
-  words_written = 0
-  combined.uniq.sort.each do |word|
-    file.puts word
-    puts "Line:#{words_written += 1} #{word.colorize(:yellow)}"
+      File.open(output_file, 'w') do |file|
+        words_written = 0
+        combined.uniq.sort.each do |word|
+          file.puts word
+          puts "Line:#{words_written += 1} #{word.colorize(:yellow)}"
+        end
+      end
+    end
   end
 end
